9b0959c114faff73e0538772b7d28c147406a622
r-mutax <magicspell.max.mix.mutax@gmail.com>
2022-09-19 11:39:24 +0900

    day02 GetMemoryMap

--- MikanXLoaderPkg/build_loader.sh
+++ MikanXLoaderPkg/build_loader.sh
@@ -23,4 +23,4 @@ sudo mkdir -p mnt/EFI/BOOT
 sudo cp BOOTX64.EFI mnt/EFI/BOOT/BOOTX64.EFI
 sudo umount mnt
 
-qemu-system-x86_64 -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd -hda disk.img
+qemu-system-x86_64 -drive if=pflash,file=$HOME/osbook/devenv/OVMF_CODE.fd -drive if=pflash,file=$HOME/osbook/devenv/OVMF_VARS.fd -hda disk.img -monitor stdio
