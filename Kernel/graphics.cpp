83ea73f7dfaecc0c6de8815c2f6d1031ca934f9a
r-mutax <magicspell.max.mix.mutax@gmail.com>
2022-09-23 21:10:54 +0900

    day06 Design desktop

--- Kernel/graphics.cpp
+++ Kernel/graphics.cpp
@@ -13,3 +13,23 @@ void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
     p[1] = c.g;
     p[2] = c.r;
 }
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos, const Vector2D<int>& size, const PixelColor& c){
+    for (int dy = 0; dy < size.y; ++dy){
+        for (int dx = 0; dx < size.x; ++dx){
+            writer.Write(dx + pos.x, dy + pos.y, c);
+        }
+    }
+}
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos, const Vector2D<int>& size, const PixelColor& c){
+    for(int dx = 0; dx < size.x; ++dx){
+        writer.Write(dx + pos.x, pos.y, c);
+        writer.Write(dx + pos.x, pos.y + size.y - 1, c);
+    }
+
+    for(int dy = 0; dy < size.y; ++dy){
+        writer.Write(pos.x, dy + pos.y, c);
+        writer.Write(pos.x + size.x - 1, dy + pos.y, c);
+    }
+}
--- Kernel/main.cpp
+++ Kernel/main.cpp
@@ -7,6 +7,9 @@
 #include "font.hpp"
 #include "console.hpp"
 
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
 const int kMouseCursorWidth = 15;
 const int kMouseCursorHeight = 24;
 const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
@@ -77,14 +80,40 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
             break;
     }
 
-    console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
-
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x){
         for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y){
             pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
 
+    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+    const int kFrameHight = frame_buffer_config.vertical_resolution;
+
+    // 背景の描画
+    FillRectangle(*pixel_writer,
+                    {0, 0},
+                    {kFrameWidth, kFrameHight - 50},
+                    kDesktopBGColor);
+
+    // タスクバーの描画
+    FillRectangle(*pixel_writer,
+                    {0, kFrameHight - 50},
+                    {kFrameWidth, 50},
+                    {1, 8, 17});
+
+    // スタートボタンのあたり？
+    FillRectangle(*pixel_writer,
+                    {0, kFrameHight - 50},
+                    {kFrameWidth / 5, 50},
+                    {80, 80, 80});
+
+    // スタートボタンの枠？
+    FillRectangle(*pixel_writer,
+                    {10, kFrameHight - 40},
+                    {30, 30},
+                    {160, 160, 160});
+
+    // マウスカーソルの描画
     for(int dy = 0; dy < kMouseCursorHeight; ++dy){
         for(int dx = 0; dx < kMouseCursorWidth; ++dx){
             if(mouse_cursor_shape[dy][dx] == '@'){
@@ -94,6 +123,9 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
             }
         }
     }
+
+    console = new(console_buf) Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
+    printk("Welcome to MikanOS_X\n");
 
     while(1) __asm__("hlt");
 }
