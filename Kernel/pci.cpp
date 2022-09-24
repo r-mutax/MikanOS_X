cc1ca07fc6c7572f61a41499c03eca0d9c6b1ada
r-mutax <magicspell.max.mix.mutax@gmail.com>
2022-09-24 22:17:41 +0900

    day06 fix make usb driver.

---
+++ Kernel/pci.cpp
@@ -0,0 +1,5 @@
+#include "pci.hpp"
+
+uint32_t MakeAddress(uint8_t buf, uint8_t device, uint8_t function, uint8_t reg_addr){
+    auto shl = 1;
+}
---
+++ Kernel/pci.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <cstdint>
+
+uint32_t MakeAddress(uint8_t buf, uint8_t device, uint8_t function, uint8_t reg_addr);
