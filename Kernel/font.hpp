636162e8ab23acf27156d63499c0053c51adec75
r-mutax <magicspell.max.mix.mutax@gmail.com>
2022-10-09 23:39:25 +0900

    day09 acceleration draw layer.

--- Kernel/font.hpp
+++ Kernel/font.hpp
@@ -4,5 +4,5 @@
 #include "graphics.hpp"
 
 extern const uint8_t kFontA[16];
-void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
-void WriteString(PixelWriter& writer, int x, int y ,const char* s, const PixelColor& color);
+void WriteAscii(PixelWriter& writer, Vector2D<int> pos, char c, const PixelColor& color);
+void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const PixelColor& color);
--- Kernel/window.hpp
+++ Kernel/window.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "graphics.hpp"
+#include "frame_buffer.hpp"
+#include "logger.hpp"
 
 #include <vector>
 #include <optional>
@@ -10,8 +12,8 @@ class Window {
         class WindowWriter : public PixelWriter {
             public:
                 WindowWriter(Window& window) : window_{window} {}
-                virtual void Write(int x, int y, const PixelColor& c) override{
-                    window_.At(x, y) = c;
+                virtual void Write(Vector2D<int> pos, const PixelColor& c) override{
+                    window_.Write(pos, c);
                 }
                 virtual int Width() const override { return window_.Width(); }
                 virtual int Height() const override { return window_.Height(); }
@@ -19,17 +21,18 @@ class Window {
                 Window& window_;
         };
 
-        Window(int width, int height);
+        Window(int width, int height, PixelFormat shadow_format);
         ~Window() = default;
         Window(const Window& rhs) = delete;
         Window& operator=(const Window& rhs) = delete;
 
-        void DrawTo(PixelWriter& writer, Vector2D<int> position);
+        void DrawTo(FrameBuffer& dst, Vector2D<int> position);
         void SetTransparentColor(std::optional<PixelColor> c);
         WindowWriter* Writer();
 
-        PixelColor& At(int x, int y);
-        const PixelColor& At(int x, int y) const;
+        const PixelColor& At(Vector2D<int> pos) const;
+        void Write(Vector2D<int> pos, PixelColor c);
+
         int Width() const ;
         int Height() const ;
     
@@ -38,4 +41,6 @@ class Window {
         std::vector<std::vector<PixelColor>> data_{};
         WindowWriter writer_{*this};
         std::optional<PixelColor> transparent_color_{std::nullopt};
+
+        FrameBuffer shadow_buffer_{};
 };
