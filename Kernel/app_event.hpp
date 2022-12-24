deaf4e9fd809fb09b5e6079bd877000e61245ebd
r-mutax <magicspell.max.mix.mutax@gmail.com>
2022-12-24 23:23:26 +0900

    Add ReadEvents "MouseButton".

--- Kernel/app_event.hpp
+++ Kernel/app_event.hpp
@@ -8,14 +8,21 @@ struct AppEvent {
     enum Type{
         kQuit,
         kMouseMove,
+        kMouseButton,
     } type;
 
     union {
-        struct{
+        struct {
             int x, y;
             int dx, dy;
             uint8_t buttons;
         } mouse_move;
+
+        struct {
+            int x, y;
+            int press;  // 1 : press, 0 : release
+            int button;
+        } mouse_button;
     } arg;
 };
 
