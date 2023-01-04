bb8977288315a164565fc7b4ba330a81ad824669
r-mutax <magicspell.max.mix.mutax@gmail.com>
2023-01-04 17:42:25 +0900

    day26 Add "@stdin".

--- Kernel/fat.hpp
+++ Kernel/fat.hpp
@@ -4,6 +4,8 @@
 #include <cctype>
 #include <utility>
 
+#include "file.hpp"
+
 namespace fat{
     struct BPB {
     uint8_t jump_boot[3];
@@ -89,10 +91,10 @@ namespace fat{
 
     size_t LoadFile(void* buf, size_t len, const DirectoryEntry& entry);
 
-    class FileDescriptor {
+    class FileDescriptor : public ::FileDescriptor{
         public:
             explicit FileDescriptor(DirectoryEntry& fat_entry);
-            size_t Read(void* buf, size_t len);
+            size_t Read(void* buf, size_t len) override;
         private:
             DirectoryEntry& fat_entry_;
             size_t rd_off_ = 0;
